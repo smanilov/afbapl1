a89cba0955ab1f0b4236d9bba484ca8c81b6b8c6
Stan Manilov <stanislav.manilov@gmail.com>
2022-01-24 18:36:31 +0200

    Оразмери бутона след зареждане на страницата

--- init.js
+++ init.js
@@ -1,7 +1,16 @@
 import { configureSourcePersistance } from './source_area.js';
 import { configureIOEditBehavior } from './io_area.js';
 
+function resizeButtonToMatchTextareas() {
+  const textarea = $("#sourceCode")[0];
+  const button = $("#runButton")[0];
+  button.style.width = textarea.offsetWidth + "px";
+}
+
 export function init() {
   configureSourcePersistance();
   configureIOEditBehavior();
+  window.addEventListener('load', function() {
+    resizeButtonToMatchTextareas();
+  });
 }
