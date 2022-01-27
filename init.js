96b0d800e10fcd2d24bd4ebc83448bfb3cb3fa9b
Stan Manilov <stanislav.manilov@gmail.com>
2022-01-27 15:41:28 +0200

    Смяна на textarea с div contenteditable

--- init.js
+++ init.js
@@ -1,16 +1,21 @@
 import { configureSourcePersistance } from './source_area.js';
 import { configureIOEditBehavior } from './io_area.js';
 
-function resizeButtonToMatchTextareas() {
-  const textarea = $("#sourceCode")[0];
+function resizeAllToMatchTextarea() {
   const button = $("#runButton")[0];
+  const div = $("#sourceCode")[0];
+  const textarea = $("#ioText")[0];
+  
   button.style.width = textarea.offsetWidth + "px";
+
+  // Subtract border and padding for the div.
+  div.style.width = textarea.offsetWidth - 6 + "px";
 }
 
 export function init() {
   configureSourcePersistance();
   configureIOEditBehavior();
   window.addEventListener('load', function() {
-    resizeButtonToMatchTextareas();
+    resizeAllToMatchTextarea();
   });
 }
--- source_area.js
+++ source_area.js
@@ -1,9 +1,47 @@
+// Converts the innerHtml of the contenteditable div that contains the source
+// to lines separated by '\n'.
+function stripTags(sourceHtml) {
+  // Basically, replace all </div>, <br>, and </p> to '\n', while stripping
+  // all <div>, and <p>.
+  var sourceCode = sourceHtml;
+  sourceCode = sourceCode.replaceAll('<div>', '');
+  sourceCode = sourceCode.replaceAll('<p>', '');
+
+  sourceCode = sourceCode.replaceAll('</div>', '\n');
+  sourceCode = sourceCode.replaceAll('</p>', '\n');
+  sourceCode = sourceCode.replaceAll('<br>', '\n');
+
+  // It turns out that spaces are also a problem.
+  sourceCode = sourceCode.replaceAll('&nbsp;', ' ');
+
+  sourceCode = sourceCode.trim();
+  return sourceCode;
+}
+
+// The inverse of stripTags... to some extent.
+function addTags(sourceCode) {
+  // Just replace '\n' with </div><div> and add <div> at the start and </div>
+  // at the end.
+  var sourceHtml = sourceCode;
+
+  sourceHtml = sourceHtml.replaceAll('\n', '</div><div>');
+  sourceHtml = '<div>' + sourceHtml + '</div>';
+
+  // Preserve indentations and wide spaces in general.
+  sourceHtml = sourceHtml.replaceAll('  ', '&nbsp; ');
+
+  // Do it again, in case there was an odd number of spaces anywhere.
+  sourceHtml = sourceHtml.replaceAll('  ', '&nbsp; ');
+
+  return sourceHtml;
+}
+
 export function getSource() {
-  return $("#sourceCode")[0].value;
+  return stripTags($("#sourceCode")[0].innerHTML);
 }
 
 function setSource(source/*: string*/) {
-  $("#sourceCode")[0].value = source;
+  $("#sourceCode")[0].innerHTML = addTags(source);
 }
 
 // Keeps source across page reloads.
